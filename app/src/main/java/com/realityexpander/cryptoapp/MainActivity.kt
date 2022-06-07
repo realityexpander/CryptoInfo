cdd2d8e682bcf461fd697ebc574f2fc1cee56526
Chris Athanas <realityexpander@gmail.com>
2022-06-06 23:03:58 -0500

    initial commit

---
+++ app/src/main/java/com/realityexpander/cryptoapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.realityexpander.cryptoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.realityexpander.cryptoapp.ui.theme.CryptoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptoAppTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    
+                }
+            }
+        }
+    }
+}
